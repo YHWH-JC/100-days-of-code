882ada62169ea9e1d8aa27ee74ba9a1e2a2a8729
YHWH-JC <211738635+YHWH-JC@users.noreply.github.com>
2026-03-30 16:27:06 -0400

    Add census , exodus, type, and variables

---
+++ census.py
@@ -0,0 +1,5 @@
+user_name = input("What is your name? ")
+user_tribe = input("What tribe are you from? ")
+user_age = input("How old are you? ")
+
+print(f"Welcome {user_name}! The tribe of {user_tribe} is blessed to have a {user_age} year old join the camp.")
---
+++ type.py
@@ -0,0 +1,7 @@
+builder = "Noah"
+days_of_rain = 40
+ark_length = 300.5
+is_flooding = True
+
+print(days_of_rain)
+print(builder + " waited for " + str(days_of_rain) + " days.")
