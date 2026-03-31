38643d83819ee114710b9e3d0a8176b28096775f
YHWH-JC <211738635+YHWH-JC@users.noreply.github.com>
2026-03-30 21:57:48 -0400

    Add guard.py

---
+++ guard.py
@@ -0,0 +1,7 @@
+secret_word = "Shalom"
+user_answer = input("Halt! Speak the secret word to enter: ")
+
+if secret_word == user_answer:
+    print("The curtain opens. Enter the Holy Place.")
+else:
+    print("Access denied! Step away from the curtain!")
