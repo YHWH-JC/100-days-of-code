31f86dd23a183ddbe51ac996a03d251d8892d1de
YHWH-JC <211738635+YHWH-JC@users.noreply.github.com>
2026-09-08 21:00:14 -0400

    create  soup_or_no_soup.py

---
+++ soup_or_no_soup.py
@@ -0,0 +1,5 @@
+name = input("Please tell me your name: ")
+if name != "Jerry":
+    portions_of_soup = float(input("How many portions of soup?"))
+    print(f"The total cost is {portions_of_soup * 5.90}")
+print("Next Please!")
